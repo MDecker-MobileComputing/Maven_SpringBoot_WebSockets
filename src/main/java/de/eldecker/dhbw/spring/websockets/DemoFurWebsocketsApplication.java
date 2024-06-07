7a3ad1b9a7d2c4ad65b7f88c955cb21733c58fea
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-07 18:16:09 +0200

    Projekt mit Spring Initializr erzeugt

---
+++ src/main/java/de/eldecker/dhbw/spring/websockets/DemoFurWebsocketsApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.websockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFurWebsocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFurWebsocketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eldecker/dhbw/spring/websockets/DemoFurWebsocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.websockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFurWebsocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
