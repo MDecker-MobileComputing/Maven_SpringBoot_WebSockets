ba42bb53e3a0815428c29dba9c6330f4b590d488
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-08 17:34:04 +0200

    WebSocketConfig: Event-Handler fuer Verbindungsaufbau/abbau

--- src/main/java/de/eldecker/dhbw/spring/websockets/ws/WebSocketConfig.java
+++ src/main/java/de/eldecker/dhbw/spring/websockets/ws/WebSocketConfig.java
@@ -1,10 +1,19 @@
 package de.eldecker.dhbw.spring.websockets.ws;
 
+import static org.springframework.messaging.simp.stomp.StompHeaderAccessor.wrap;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 
@@ -19,6 +28,11 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    
+    private final static Logger LOG = LoggerFactory.getLogger( WebSocketConfig.class );
+    
+    /** Collection, die die IDs der aktiven Sitzungen speichert. */
+    private Set<String> _sitzungsSet = new HashSet<>( 10 );
 
     /**
      * STOMP-Endpunkt {@code /ws} registrieren.
@@ -28,5 +42,66 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
         registry.addEndpoint( "/ws" ).withSockJS();
     }
+    
+    
+    /**
+     * Event-Handler für Verbindung neuer Websocket-Client, schreibt Sitzungs-ID auf
+     * Logger.
+     * 
+     * @param event Event für neue Sitzung
+     */
+    @EventListener
+    public void handleSessionConnected( SessionConnectEvent event ) {
+        
+        final StompHeaderAccessor sha = wrap( event.getMessage() );
+        
+        final String sitzungsID = sha.getSessionId(); 
+        
+        final boolean nochNichtDa = _sitzungsSet.add(sitzungsID);
+        if ( ! nochNichtDa ) {
+            
+            LOG.warn( "Sitzungs-ID \"{}\" für \"neue\" Verbindung war schon bekannt!", 
+                      sitzungsID );             
+        }
+        
+        LOG.info( "Neuer Client mit Sitzungs-ID \"{}\" hat sich verbunden.", sitzungsID );                         
+        
+        loggeAnzahlVerbindungen();
+    }
+    
+    
+    /**
+     * Event-Handler für den Fall, dass ein Websocket-Client die Verbindung
+     * beendet.
+     * 
+     * @param event Event für beendete Sitzung
+     */
+    @EventListener
+    public void handleSessionDisconnect( SessionDisconnectEvent event ) {
+        
+        final StompHeaderAccessor sha = wrap( event.getMessage() );
+        
+        final String sitzungsID = sha.getSessionId();
+                
+        final boolean wurdeEntfernt = _sitzungsSet.remove( sitzungsID );
+        if ( ! wurdeEntfernt ) {
+            
+            LOG.warn( "Sitzungs-ID \"{}\" von beendeter Verbindung war nicht bekannt!", 
+                      sitzungsID );
+        }
+        
+        LOG.info( "Client mit Sitzungs-ID \"{}\" hat Verbindung beendet.", sitzungsID );                   
+        
+        loggeAnzahlVerbindungen();
+    }    
 
+    
+    /**
+     * Schreibt aktuelle Anzahl der Verbindungen auf den Logger.
+     */
+    private void loggeAnzahlVerbindungen() {
+        
+        LOG.info( "Aktuelle Anzahl Verbindungen: {}", _sitzungsSet.size() );
+    }
+    
 }
