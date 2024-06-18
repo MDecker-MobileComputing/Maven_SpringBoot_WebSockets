8cf341490ca4bd934e9be670612e2eb6f38804d5
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-18 13:57:59 +0200

    LocalDateTimeStringConverter: Javadoc dazu

--- src/main/java/de/eldecker/dhbw/spring/websockets/db/LocalDateTimeStringConverter.java
+++ src/main/java/de/eldecker/dhbw/spring/websockets/db/LocalDateTimeStringConverter.java
@@ -11,18 +11,33 @@ import java.time.format.DateTimeParseException;
 /**
  * Diese Klasse sorgt dafür, dass die Attribute vom Typ {@code LocalDateTime}
  * als String im gut lesbaren Format gespeichert werden, z.B. {@code 23. Mai 2024, 23:10:03}.
+ * 
+ * @param <LocalDateTime> Datentyp für Attribute in Entity-Klassen, auf denen dieser Konverter
+ *                        angewendet wird (wegen {@code autoApply = true} in der Annotation
+ *                        {@code Converter} wird dies automatisch für alle Attribute von
+ *                        diesem Typ in Entity-Klassen gemacht.
+ *                        
+ * @param <String> Typ der Datenbankspalte
  */
 @Converter( autoApply = true )
 public class LocalDateTimeStringConverter implements AttributeConverter<LocalDateTime, String> {
 
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern( "dd.MM.yyyy, HH:mm:ss" );
 
+    /**
+     * Diese Methode wird vor dem Speichern eines Attributs vom Typ {@code LocalDateTime} auf die Datenbank
+     * aufgerufen, um es in einen String umzuwandeln.
+     */
     @Override
     public String convertToDatabaseColumn( LocalDateTime attribute ) {
         
         return attribute != null ? attribute.format( formatter ) : null;
     }
 
+    /**
+     * Diese Methode wird aufgerufen, wenn ein Attributwert, der beim Speichern in einen String umgewandelt
+     * wurde, wieder eingelesen werden soll.
+     */
     @Override
     public LocalDateTime convertToEntityAttribute( String dbData ) {
         
