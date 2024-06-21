c41af482c2d13009cb6faf64d0b81ef6bb6c6c02
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-21 13:17:21 +0200

    Update addons_papertrail_create.bat

--- heroku/addons_papertrail_create.bat
+++ heroku/addons_papertrail_create.bat
@@ -1,13 +1,16 @@
 
 @REM https://elements.heroku.com/addons/papertrail
 
+@REM Mit dem in Heroku eingebauten Log (siehe logTails.bat) sind nur die letzten 1500 Zeilen verfügbar.
+
 heroku addons:create papertrail:choklad
 @REM choklad: Kostenloser Tarif (Search Duration: 2 Tage; Archive Duration: 7 Tage)
 
-@REM Danach noch Papertrail-Plugin für Heroku installieren:
+@REM Über Dashboard kann zu Web-UI von Papertrail auf https://my.papertrailapp.com/
+@REM abgesprungen werden, oder über den folgenden CLI-Befehl: heroku addons:open papertrail
+
+@REM Danach kann man noch Papertrail-Plugin für Heroku-CLI installieren:
 @REM heroku plugins:install heroku-papertrail
 @REM
 @REM Logfile herunterladen: heroku pt > papertrail.log
 @REM In Logfile suchen: heroku pt -t "search string"
-@REM 
-@REM Mit dem in Heroku eingebauten Log (siehe logTails.bat) sind nur die letzten 1500 Zeilen verfügbar.
